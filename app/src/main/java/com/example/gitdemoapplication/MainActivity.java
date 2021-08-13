831a3c23c7ae4e7750f04d0c8afefb0ac175c991
yue <3417771078@qq.com>
2021-08-13 15:02:47 +0800

    implement:btnAdd onClick Listener

--- app/src/main/java/com/example/gitdemoapplication/MainActivity.java
+++ app/src/main/java/com/example/gitdemoapplication/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.gitdemoapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnAdd;
+    private TextView textView;
+    private int number=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd=findViewById(R.id.btnAdd);
+        textView=findViewById(R.id.textView);
+        textView.setText(String.valueOf(number));
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(String.valueOf(number++));
+            }
+        });
     }
 }
