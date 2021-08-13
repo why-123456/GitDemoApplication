e30c3fe5d4659e1aca690546c70e4eea74e7ff21
yue <3417771078@qq.com>
2021-08-13 18:20:18 +0800

    通过viewmodel实现了旋转屏幕丢失数据的bug

--- app/src/main/java/com/example/gitdemoapplication/MainActivity.java
+++ app/src/main/java/com/example/gitdemoapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gitdemoapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,28 +17,33 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btnAdd,btnSub;
     private TextView textView;
-    private int number=0;
+//    private int number=0;
+    private MyViewModel myViewModel;
+    private int myNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState!=null){
-            number=savedInstanceState.getInt("num");
-        }
+        //和ViewModel的效果一样的
+//        if (savedInstanceState!=null){
+//            number=savedInstanceState.getInt("num");
+//        }
+        myViewModel=new ViewModelProvider(this).get(MyViewModel.class);
+//        myNum=myViewModel.getNumber();
         btnAdd=findViewById(R.id.btnAdd);
         btnSub=findViewById(R.id.btnSub);
         textView=findViewById(R.id.textView);
-        textView.setText(String.valueOf(number));
+        textView.setText(String.valueOf(myViewModel.getNumber()));
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(String.valueOf(++number));
+                textView.setText(String.valueOf(myViewModel.setNumber(++myNum)));
             }
         });
         btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(String.valueOf(--number));
+                textView.setText(String.valueOf(myViewModel.setNumber(--myNum)));
             }
         });
     }
@@ -44,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId()==R.id.reset){
-            number=0;
-            textView.setText(String.valueOf(number));
+            myNum=0;
+            textView.setText(String.valueOf(myNum));
         }
         return super.onOptionsItemSelected(item);
     }
@@ -56,9 +63,9 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    protected void onSaveInstanceState( Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("num",number);
-    }
+//    @Override
+//    protected void onSaveInstanceState( Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putInt("num",number);
+//    }
 }
--- app/src/main/java/com/example/gitdemoapplication/MyViewModel.java
+++ app/src/main/java/com/example/gitdemoapplication/MyViewModel.java
@@ -4,4 +4,20 @@ import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
     private int number=0;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int setNumber(int number) {
+        this.number = number;
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "MyViewModel{" +
+                "number=" + number +
+                '}';
+    }
 }
