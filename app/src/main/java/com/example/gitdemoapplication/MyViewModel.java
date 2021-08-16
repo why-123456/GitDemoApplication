cd33b913d3ad6fdf9548ea32df2dec516e9a9bc0
yue <3417771078@qq.com>
2021-08-16 09:51:41 +0800

    重构：在viewmodel中添加livedata

--- app/src/main/java/com/example/gitdemoapplication/MyViewModel.java
+++ app/src/main/java/com/example/gitdemoapplication/MyViewModel.java
@@ -1,14 +1,17 @@
 package com.example.gitdemoapplication;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
     private int number=0;
-
+    private MutableLiveData<Integer> mNameEvent=new MutableLiveData<>();
     public int getNumber() {
         return number;
     }
-
+    public MutableLiveData<Integer> getNameEvent() {
+        return mNameEvent;
+    }
     public int setNumber(int number) {
         this.number = number;
         return number;
